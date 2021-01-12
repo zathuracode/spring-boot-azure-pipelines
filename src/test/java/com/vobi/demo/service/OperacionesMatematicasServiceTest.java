c69140eaacbbd6e410ce0ab8d86b3080390ccfcc
Diego Armando Gomez Mosquera <dgomez@Diegos-MacBook-Pro.local>
2021-01-11 19:27:10 -0500

    Se usa la injeccion de dependencias en la prueba

--- src/test/java/com/vobi/demo/service/OperacionesMatematicasServiceTest.java
+++ src/test/java/com/vobi/demo/service/OperacionesMatematicasServiceTest.java
@@ -3,12 +3,16 @@ package com.vobi.demo.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 
-
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class OperacionesMatematicasServiceTest {
 
-	OperacionesMatematicasService operacionesMatematicasService=new OperacionesMatematicasServiceImpl();
+	@Autowired
+	OperacionesMatematicasService operacionesMatematicasService;
 	
 	@Test
 	public void debeSumar()throws Exception {
